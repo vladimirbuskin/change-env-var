54f8286a848cb8fdae15e1015401112957eb523d
Vladimir Buskin <vladimir.buskin@gmail.com>
2017-07-24 01:33:21 +0400

    removed test file

--- 1.js
+++
@@ -1,9 +0,0 @@
-debugger;
-
-var s = require('./envVarUtil')
-var changeEnvVar = require('./index.core')(s.envVarRead, s.envVarWrite)
-
-changeEnvVar.addVariable('pm2', 'i1', function() {
-  console.log('success')
-})
-
